21eb0ed7da4fbb549932b67ed208eee57eb1549e
Victor Mazo <viamazoba@unal.edu.co>
2025-04-09 07:28:19 -0500

    feat: Se realiza primera versión de la pantalla dde permisos para simular dichas peticiones

--- src/presentation/screens/permissions/PermissionsScreen.tsx
+++ src/presentation/screens/permissions/PermissionsScreen.tsx
@@ -1,11 +1,24 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
+import { globalStyles } from "../../../config/theme/styles";
+import { usePermissionStore } from "../../store/permissions/usePermissionStore";
 
 
 export const PermissionsScreen = () => {
 
+    const { locationStatus, requestLocationPermission } = usePermissionStore();
+
     return (
-        <View>
-            <Text>PermissionsScreen</Text>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Habilitar ubicación</Text>
+
+            <Pressable
+                style={globalStyles.btnPrimary}
+                onPress={requestLocationPermission}
+            >
+                <Text style={{ color: 'white' }}>Habilitar localización</Text>
+            </Pressable>
+
+            <Text>Estado actual: {locationStatus}</Text>
         </View>
     );
 };
